27b37ffb46a76f57d2f725cf963afe586fe4eb8d
icchon <icchon1001@gmail.com>
2026-05-20 14:12:32 +0000

    bug: delete big space

--- p3/router/leaves/leaf2.sh
+++ p3/router/leaves/leaf2.sh
@@ -6,7 +6,7 @@ IP_ADDR=10.1.1.6/30
 ip addr flush dev lo
 ip addr flush dev eth0
 
-ip addr add ${LOOPBACK_ADDR}${LOOPBACK_MASK}  dev lo
+ip addr add ${LOOPBACK_ADDR}${LOOPBACK_MASK} dev lo
 ip addr add ${IP_ADDR} dev eth0
 ip link set eth0 up
 
